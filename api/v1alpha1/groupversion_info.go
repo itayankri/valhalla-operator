fa70a65b97b081e596bc39129c892dea7e5c6b86
Itay Ankri <itay.ankri@gmail.com>
2022-10-12 21:53:07 +0300

    add controller-gen

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the valhalla v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=valhalla.itayankri
+// +kubebuilder:object:generate=true
+// +groupName=valhalla.itayankri
 package v1alpha1
 
 import (
