0e24b925cd1b3dfbfcd251d306e5fa61b0af1979
David Peck <david@littlemonkey.co.nz>
2023-02-02 20:56:20 +0000

    make docker images configurable

---
+++ api/v1alpha1/constants.go
@@ -0,0 +1,5 @@
+package v1alpha1
+
+const workerImage = "itayankri/valhalla-worker:latest"
+const mapBuilderImage = "itayankri/valhalla-builder:latest"
+const historicalTrafficDataFetcherImage = "itayankri/valhalla-predicted-traffic:latest"
--- internal/resource/constants.go
+++ internal/resource/constants.go
@@ -1,9 +1,6 @@
 package resource
 
 const valhallaDataPath = "/data"
-const workerImage = "itayankri/valhalla-worker:latest"
-const mapBuilderImage = "itayankri/valhalla-builder:latest"
-const hirtoricalTrafficDataFetcherImage = "itayankri/valhalla-predicted-traffic:latest"
 
 const DeploymentSuffix = ""
 const HorizontalPodAutoscalerSuffix = ""
